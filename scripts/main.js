19aa5c5a5294c6ad57aa595d3860d0eebd7908c2
freakolope <akashmulye97@gmail.com>
2019-05-03 09:33:42 -0400

    fifth commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 var cursorText = $("#cursor-text");
+var cursorPlaceHolder = "Project 1";
 var cursorTextPos = cursorText.position();
+
+var projectsCounter = 0;
 
 function getPos(e) {
 
@@ -12,21 +15,91 @@ function getPos(e) {
 document.onmousemove = function(e) {
 
 	var PosCords = getPos(e);
-	console.log("move MOUSE: " + PosCords.x1 + " |||| " + PosCords.y1 + "move ELEM: " + PosCords.x2 + " |||| " + PosCords.y2);
+	// console.log("move MOUSE: " + PosCords.x1 + " |||| " + PosCords.y1 + "move ELEM: " + PosCords.x2 + " |||| " + PosCords.y2);
 
 	cursorText.animate({
 		left: PosCords.x1,
 		top: PosCords.y1
 	}, 0, "linear");
+
+	if (PosCords.x1 >= 800 && PosCords.y1 <= 400) {
+
+		cursorText.text("Next Project");
+	
+	} else if (PosCords.x1 <= 600 && PosCords.y1 <= 400) {
+
+		cursorText.text("Previous Project");
+	
+	} else {
+
+		cursorText.text("Project 1");
+	}
 }
 
-$("#nav-cap-span, #snippets-span").on("mouseenter", function() {
+$("#snippets-span").on("mouseenter", function() {
 
-	cursorText.fadeOut(500);
+	cursorText.text("snippets");
 });
 
-$("#nav-cap-span, #snippets-span").on("mouseleave", function() {
+$("#snippets-span").on("mouseleave", function() {
 
-	cursorText.fadeIn(100);
+	cursorText.text(cursorPlaceHolder);
 });
+
+$("#nav-cap-span").on("mouseenter", function() {
+
+	cursorText.text("menu");
+});
+
+$("#nav-cap-span").on("mouseleave", function() {
+
+	cursorText.text(cursorPlaceHolder);
+});
+
+document.onmousedown = function(e) {
+
+	var angle = 0;
+
+	var PosCords = getPos(e);
+
+	if (PosCords.x1 >= 800) {
+
+		if (projectsCounter == 0) {
+
+			$("#banner-image").animate({opacity: "0"}, 100).delay(100).queue(function(next) {
+				$(this).attr("src", "media/folder2/f2-img1.jpg"); next();
+			}).delay(100).animate({opacity: "1"}, 100);
+
+			cursorText.css({"color" : "#171E26"});
+
+			projectsCounter++;
+		
+		} else if (projectsCounter == 1) {
+
+				$("#banner-image").animate({opacity: "0"}, 100).delay(100).queue(function(next) {
+					$(this).attr("src", "media/folder3/f3-img1.jpg"); next();
+
+					if ($(this).attr("src") === "media/folder3/f3-img1.jpg") {
+
+						$("#banner-image").css({
+							
+							"webkitTransform" : "rotate(-90deg)",
+							"MozTransform" : "rotate(-90deg)",
+							"OTransform" : "rotate(-90deg)",
+							"transform" : "rotate(-90deg)"
+						});
+					}
+
+				}).delay(100).animate({opacity: "1"}, 100);
+
+			cursorText.css({"color" : "#171E26"});
+
+			projectsCounter++;
+		}
+	
+	} else if (PosCords.x1 <= 600) {
+
+		console.log("left");
+	}
+}
 
